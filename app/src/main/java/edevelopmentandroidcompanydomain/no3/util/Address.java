fc649755d46ef1209001af79cf7dea4cd8f47c52
lucky <632496308@qq.com>
2019-03-04 17:24:35 +0800

    master  新增Address.class 文件 可以进行下一次合并

---
+++ app/src/main/java/edevelopmentandroidcompanydomain/no3/util/Address.java
@@ -0,0 +1,24 @@
+package edevelopmentandroidcompanydomain.no3.util;
+
+/**
+ * Created by lucky on 2018/12/9.
+ */
+
+public class Address {
+    public final static String SetCarMove= "https://party.jiqiaodashi.top/transportservice/type/jason/action/SetCarMove.do";
+    public final static String GetCarAccountBalance= "https://party.jiqiaodashi.top/transportservice/type/jason/action/GetCarAccountBalance.do";
+    public final static String SetCarAccountRecharge= "https://party.jiqiaodashi.top/transportservice/type/jason/action/SetCarAccountRecharge.do";
+    public final static String GetCarSpeed= "https://party.jiqiaodashi.top/transportservice/type/jason/action/GetCarSpeed.do";
+    public final static String SetTrafficLightNowStatus= "https://party.jiqiaodashi.top/transportservice/type/jason/action/SetTrafficLightNowStatus.do";
+    public final static String GetTrafficLightConfigAction= "https://party.jiqiaodashi.top/transportservice/type/jason/action/GetTrafficLightConfigAction.do";
+    public final static String GetAllSense= "https://party.jiqiaodashi.top/transportservice/type/jason/action/GetAllSense.do";
+    public final static String GetParkRate= "https://party.jiqiaodashi.top/transportservice/type/jason/action/GetParkRate.do";
+    public final static String SetParkRate= "https://party.jiqiaodashi.top/transportservice/type/jason/action/SetParkRate.do";
+    public final static String GetRoadStatus= "https://party.jiqiaodashi.top/transportservice/type/jason/action/GetRoadStatus.do";
+    public final static String GetParkFree= "https://party.jiqiaodashi.top/transportservice/type/jason/action/GetParkFree.do";
+    public final static String GetLightSenseValve= "https://party.jiqiaodashi.top/transportservice/type/jason/action/GetLightSenseValve.do";
+    public final static String GetBusStationInfo="https://party.jiqiaodashi.top/transportservice/type/jason/action/GetBusStationInfo.do";
+
+
+
+}
